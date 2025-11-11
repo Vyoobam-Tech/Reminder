81b0c9542204e693fd531ca0c64c34f0a62a5782
jasimahamed <jasim2003ahamed@gmail.com>
2025-11-11 11:41:45 +0530

    update

--- frontend/src/pages/EmployeePage.jsx
+++ frontend/src/pages/EmployeePage.jsx
@@ -8,19 +8,3 @@ const EmployeePage = () => {
 }
 
 export default EmployeePage
-
-// import React from "react";
-
-// // ✅ Correct relative path
-// import GroupManager from "../group/GroupManager";
-
-
-// export default function GroupPage() {
-//   return (
-//     <div style={{ padding: "20px" }}>
-      
-//       <GroupManager />
-    
-//     </div>
-//   );
-// }
