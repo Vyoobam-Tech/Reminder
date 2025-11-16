7d64a619d14e2170809eeeaa8de9a0867c25c570
jasimahamed <jasim2003ahamed@gmail.com>
2025-11-16 08:43:38 +0530

    updated

--- frontend/src/components/Remainder.jsx
+++ frontend/src/components/Remainder.jsx
@@ -228,6 +228,7 @@ import { SlBell } from "react-icons/sl";
                   <TableCell>{r.type}</TableCell>
                   <TableCell>{r.notes}</TableCell>
                   <TableCell>{r.date.replace("T", " ").slice(0, 16)}</TableCell>
+                  <TableCell>{r.recurrence}</TableCell>
                   {/* <TableCell>{(r.deliveryMethods || []).join(', ')}</TableCell> */}
                   <TableCell>
                     <Button size="large" onClick={() => handleOpen(r)}><MdEdit size={25} /></Button>
