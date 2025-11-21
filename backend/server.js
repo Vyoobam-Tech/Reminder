cfaadd623dd3147b89abd9e84abff3719f58f3ab
jasimahamed <jasim2003ahamed@gmail.com>
2025-11-21 15:33:00 +0530

    backend updated

--- backend/server.js
+++ backend/server.js
@@ -19,11 +19,23 @@ import path from 'path'
 dotenv.config();
 const app = express();
 
-const CLIENT_URL= process.env.CLIENT_URL || 'http://localhost:5173'
+// const CLIENT_URL= process.env.CLIENT_URL || 'http://localhost:5173'
 
-app.use(cors({ origin: [CLIENT_URL],
-      credentials: true,
-      methods: ['GET', "POST", "PUT", "DELETE"]}));
+// app.use(cors({ origin: [CLIENT_URL],
+//       credentials: true,
+//       methods: ['GET', "POST", "PUT", "DELETE"]}));
+
+app.use(
+  cors({
+    origin: [
+      "https://reminder.vyoobam.com",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(path.resolve("uploads")));
