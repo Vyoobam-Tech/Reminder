caad43efae0d3405e14ede52572dcb50bb9bf9d8
jasimahamed <jasim2003ahamed@gmail.com>
2025-11-21 15:51:23 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,6 @@ app.use(
   cors({
     origin: [
       "https://reminder.vyoobam.com",
-      "http://localhost:5173",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
